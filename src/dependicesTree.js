c38b1a62909f537d26d395734bd52c6e1423f04c
cjd <459953103@qq.com>
2021-06-10 19:06:02 +0800

    feat: 初创代码

---
+++ src/dependicesTree.js
@@ -0,0 +1,11 @@
+class DependiciesTree {
+  constructor() {
+    this.baseURL = process.cwd()
+    this.dependiciesMap = Object.create(null)
+  }
+  getDependiciesTree(exports, dirname) {
+    for (let key in )
+  }
+}
+
+module.exports = new DependiciesTree()
